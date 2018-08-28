427110576bbd1616d784e04eb576116ec16f6cf7
lequangcanh <lequangcanh94@gmail.com>
2018-08-28 16:47:26 +0700

    init project

---
+++ src/components/tasks/Task.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <input type="checkbox" :id="`task-${task.id}`" :checked="task.is_done" @change="handleTask" />
+    <label :for="`task-${task.id}`" class="line-through">{{task.title}}</label>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['task'],
+
+  methods: {
+    handleTask: function (e) {
+      const taskStt = {
+        task: this.task,
+        value: e.target.checked
+      }
+      this.$emit('handleTask', taskStt)
+    }
+  }
+}
+</script>
